6882df7c0594ea5d3a19abae103ecdd688ca5336
AtanuPanja <atanupanja9463@gmail.com>
2024-03-02 18:59:07 +0530

    Added image and basic drawer

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,67 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var screenWidth = MediaQuery.of(context).size.width;
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (context) => InkWell(
+            child: Container(
+              width: 300,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 0, 115, 234),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              child: Image.asset(
+                'assets/images/logo.png',
+                scale: 0.5,
+              ),
+                  
+            ),
+            onTap: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+          ),
+        ),
         body: Center(
           child: Text('Hello World!'),
+        ),
+        drawer: Drawer(
+
+          width: screenWidth - 50,
+          elevation: 2,
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.all(32.0),
+                width: 300,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 0, 115, 234),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              ElevatedButton(
+                onPressed: (){}, 
+                child: Row(
+                  children: [
+                    Icon(Icons.add),
+                    Text('New Project'),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.card_travel_rounded),
+                title: Text('Projects'),
+              ),
+            ],
+          ),
         ),
       ),
     );
