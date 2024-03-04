34ac7b8728e926499bf8921c5fa12094ce34a63f
AtanuPanja <atanupanja9463@gmail.com>
2024-03-04 11:19:15 +0530

    Added to appbar and drawer
    Added more list elements in the drawer,
    adjusted the logo size, and changed color
    theme for elevated buttons
    
    Added new image asset in the assets/images folder
    Added search text field, and profile icon
    in the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,64 +11,154 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
-
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(Color.fromARGB(255, 0, 115, 234)),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+            padding: MaterialStateProperty.all(
+              EdgeInsets.symmetric(
+                horizontal: 8.0,
+                vertical: 16.0,
+              ),
+            ),
+            shape: MaterialStateProperty.all(
+              BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(2.8),
+              ),
+            ),
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: Builder(
-            builder: (context) => InkWell(
-            child: Container(
-              width: 300,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 0, 115, 234),
-                  borderRadius: BorderRadius.circular(8.0),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Builder(
+              builder: (context) => InkWell(
+                child: Container(
+                  width: 300,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 0, 115, 234),
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/logo.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              child: Image.asset(
-                'assets/images/logo.png',
-                scale: 0.5,
+                onTap: () {
+                  Scaffold.of(context).openDrawer();
+                },
               ),
-                  
             ),
-            onTap: () {
-              Scaffold.of(context).openDrawer();
-            },
           ),
+          title: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              fillColor: Colors.grey.shade200,
+              filled: true,
+              hintText: 'Search',
+              suffixIcon: Icon(
+                Icons.search,
+              ),
+            ),
           ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                child: Image.asset('assets/images/default_profile_32px.png'),
+                onTap: () {},
+              ),
+            ),
+          ],
         ),
         body: Center(
           child: Text('Hello World!'),
         ),
         drawer: Drawer(
-
           width: screenWidth - 50,
           elevation: 2,
-          child: ListView(
-            children: [
-              Container(
-                margin: EdgeInsets.all(32.0),
-                width: 300,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 0, 115, 234),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              ElevatedButton(
-                onPressed: (){}, 
-                child: Row(
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Icon(Icons.add),
-                    Text('New Project'),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              // color: Color.fromARGB(255, 0, 115, 234),
+                              borderRadius: BorderRadius.circular(8.0),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/logo.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Customer Support',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Icon(Icons.add),
+                          Text('New Project'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 7),
+                    ListTile(
+                      leading: Icon(
+                        Icons.card_travel_rounded,
+                      ),
+                      title: Text('Projects'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.person_outlined,
+                      ),
+                      title: Text('Project Managers'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.person_add_alt_1_outlined,
+                      ),
+                      title: Text('Employees'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.settings_outlined,
+                      ),
+                      title: Text('Settings'),
+                      onTap: () {},
+                    ),
                   ],
                 ),
               ),
-              ListTile(
-                leading: Icon(Icons.card_travel_rounded),
-                title: Text('Projects'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
