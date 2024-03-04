fb76773d6352557b47180c52da4e370293880d00
AtanuPanja <atanupanja9463@gmail.com>
2024-03-04 18:25:08 +0530

    Added dropdown button
    Learned about dropdown button, and implemented
    it into the multi-step form.
    
    Removed the list of steps, and put the step
    elements directly into the Stepper, to enable
    access of state variable, in the dropdown
    
    Added step 2 details, in the form

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MainApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/widgets/new_project_stepper.dart
+++ lib/widgets/new_project_stepper.dart
@@ -9,39 +9,17 @@ class NewProjectStepper extends StatefulWidget {
 
 class _NewProjectStepperState extends State<NewProjectStepper> {
   int currentStep = 0;
-  List<Step> steps = [
-    Step(
-      title: Text('Project Details'),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Project name'),
-          SizedBox(height: 5,),
-          TextFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.grey.shade700,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(),
-              ),
-              hintText: 'Placeholder text here',
-            ),
-          ),
-        ],
-      ),
-    ),
-    Step(
-      title: Text('Step 2'),
-      content: Text('Content for Step 2'),
-    ),
-    Step(
-      title: Text('Step 3'),
-      content: Text('Content for Step 3'),
-    ),
+
+  String selectedOptionFromDropdown = 'Project Manager One';
+
+  List<String> listOfProjectManagers = [
+    'Project Manager One',
+    'Project Manager Two',
+    'Project Manager Three',
+    'Project Manager Four',
   ];
+
+  // there are three steps
 
   @override
   Widget build(BuildContext context) {
@@ -49,15 +27,127 @@ class _NewProjectStepperState extends State<NewProjectStepper> {
       body: Container(
         child: Stepper(
           currentStep: currentStep,
-            onStepContinue: () {
-              setState(() {
-                if (currentStep < steps.length - 1) {
-                  currentStep += 1;
-                } else {
-                }
-              });
-            },
-            steps: steps,
+          onStepContinue: () {
+            setState(() {
+              // since there are three steps, steps.length replaced with 3
+              if (currentStep < 3 - 1) {
+                currentStep += 1;
+              } else {}
+            });
+          },
+          steps: [
+            Step(
+              title: Text('Project Details'),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Project name *'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                      hintText: 'Placeholder text here',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Step(
+              title: Text('Invite client'),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Client name'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                      hintText: 'Name here',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Client email'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                      hintText: 'example@work.com',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  OutlinedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      foregroundColor: MaterialStateProperty.all(Colors.black),
+                      overlayColor: MaterialStateProperty.all(
+                          Color.fromARGB(45, 0, 113, 234)),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.add),
+                        Text('Add'),
+                      ],
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Step(
+              title: Text('Select Project Manager'),
+              content: DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down_outlined,),
+                isExpanded: true,
+                value: selectedOptionFromDropdown,
+                onChanged: (value) {
+                  setState(() {
+                    selectedOptionFromDropdown = value ?? 'Project Manager One';
+                  });
+                },
+                items: listOfProjectManagers.map<DropdownMenuItem<String>> ((String value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
         ),
       ),
     );
