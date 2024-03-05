84a6d26d44b3aadad4f824cd88030ea4af19f79d
AtanuPanja <atanupanja9463@gmail.com>
2024-03-05 12:28:40 +0530

    Created a ProjectListing widget
    Created of list of projects as list of map
    objects
    Learned about Table and TableRow widgets, and
    used the list of projects to populate elements
    in TableRow widgets inside Table widget
    
    Created a tab switching functionality, with the
    tabs 'All Projects', 'In progress', 'Completed',
    'Hold'. Currently, the switching of the tabs,
    will show the name of the tab as the heading
    inside the project listing. And, in any tab,
    all the projects are displayed.

--- lib/widgets/projects_screen_widgets.dart
+++ lib/widgets/projects_screen_widgets.dart
@@ -44,3 +44,237 @@ class ProjectsCardDisplay extends StatelessWidget {
     );
   }
 }
+
+
+class ProjectListing extends StatefulWidget {
+  const ProjectListing({super.key});
+
+  @override
+  State<ProjectListing> createState() => _ProjectListingState();
+}
+
+class _ProjectListingState extends State<ProjectListing> {
+
+  var listOfProjects = [
+    {
+      'name': 'Food on time',
+      'start_date': '12 Feb 24',
+      'status': 'On going',
+      'project_manager': 'Dipa Majumdar',
+      'members': '6'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': '1 Jan 24',
+      'status': 'Closed',
+      'project_manager': 'Dipa Majumdar',
+      'members': '12'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Dipa Majumdar',
+      'members': '14'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Dipa Majumdar',
+      'members': '4'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Rohit Shah',
+      'members': '30'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Hold',
+      'project_manager': 'Rohit Shah',
+      'members': '20'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Rohit Shah',
+      'members': '13'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Rohit Shah',
+      'members': '9'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Closed',
+      'project_manager': 'Rohit Shah',
+      'members': '100'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Closed',
+      'project_manager': 'Rohit Shah',
+      'members': '100'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Closed',
+      'project_manager': 'Rohit Shah',
+      'members': '100'
+    },
+
+  ];
+
+  String selectedTab = 'All Projects';
+  List<String> tabs = ['All Projects', 'In progress', 'Completed', 'Hold'];
+  List<String> tableHeaders = ['Project Name', 'Started on', 'Status', 'Project Manager', 'Members'];
+  // List<bool> tabSelected = [true, false, false, false];
+
+  // void updateTabs(int currentIndex) {
+  //   setState(() {
+  //     // for (var i = 0; i < tabSelected.length; i++) {
+  //     //   tabSelected[i] = false;
+  //     // }
+  //     // tabSelected[currentIndex] = true;
+  //   });
+  // }
+
+  void setSelectedTab(String tabName) {
+    setState(() {
+      selectedTab = tabName;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: tabs.map<Widget>((tabName) {
+              return InkWell(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: selectedTab == tabName ? const Color.fromARGB(255, 0, 115, 234): Colors.transparent,
+                      ),
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(tabName),
+                ),
+                onTap: () {
+                  setSelectedTab(tabName);
+                },
+              );
+            },).toList(),
+          ),
+      
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                selectedTab,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              InkWell(
+                child: Row(
+                  children: [
+                    Icon(Icons.arrow_upward,),
+                    Icon(Icons.arrow_downward,),
+                  ],
+                ),
+                onTap: () {},
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Table(
+            children: [
+              TableRow(
+                children: tableHeaders.map<Widget>((tableHeader) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      tableHeader,
+                      style: TextStyle(
+                        color: Colors.grey.shade800,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  );
+                },).toList(),
+              ),
+              
+              ...listOfProjects.map<TableRow>((project) {
+                return TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        '${project['name']}'
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        '${project['start_date']}'
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+                      decoration: BoxDecoration(
+                        color: switch (project['status']) {
+                          'On going' || 'In progress' => Color.fromARGB(255, 49, 135, 80),
+                          'Closed' => Color.fromARGB(255, 216, 58, 82),
+                          'Hold' => Colors.black,
+                          _ => Colors.transparent,
+                        },
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: Text(
+                        '${project['status']}',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        '${project['project_manager']}'
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        '${project['members']}'
+                      ),
+                    ),
+                  ],
+                );
+              },).toList()
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
