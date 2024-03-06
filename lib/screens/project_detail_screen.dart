659d17191cdac07620ee30a73928f11a1b7a5b51
AtanuPanja <atanupanja9463@gmail.com>
2024-03-06 12:57:08 +0530

    Added escalation matrix
    Created an escalation matrix table,
    containing the states of the current selection
    of persons.
    
    The dropdown would change the person name,
    and accordingly the role is updated, because
    the dropdown values are part of the state.
    
    Used three state variables, for different levels,
    to ensure we can select different person at each
    level.
    
    Currently the same table is displayed, for each
    of the matrices.

--- lib/screens/project_detail_screen.dart
+++ lib/screens/project_detail_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ProjectDetailsScreen extends StatefulWidget {
-  const ProjectDetailsScreen({super.key});
+  const ProjectDetailsScreen({
+    super.key,
+    required this.projectName,
+  });
+
+  final String projectName;
 
   @override
   State<ProjectDetailsScreen> createState() => _ProjectDetailsScreenState();
@@ -27,72 +32,85 @@ class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('Food on time'),
-            Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.grey.shade500,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.projectName,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: tabs.map<Widget>(
-                  (tabName) {
-                    return Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: currentTab == tabName
-                                ? const Color.fromARGB(255, 0, 115, 234)
-                                : Colors.transparent,
-                            width: 2,
-                          ),
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(tabName),
-                    );
-                  },
-                ).toList(),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  switch (currentTab) {
-                    'Project Overview' => ProjectOverview(),
-                    'Scope & stack' => ScopeAndStack(),
-                    _ => Text(currentTab),
-                  },
-                  SizedBox(height: 20,),
-                  ElevatedButton(
-                    onPressed: () {
-                      switch (currentTab) {
-                        case 'Project Overview':
-                          setCurrentTab('Scope & stack');
-                        case 'Scope & stack':
-                          setCurrentTab('Escalation matrix');
-                        default:
-                          return;
-                      }
-                    },
-                    child: Text('Continue'),
-                    style: ButtonStyle(
-                      minimumSize: MaterialStateProperty.all(
-                        Size(screenWidth, 50),
-                      ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey.shade500,
                     ),
                   ),
-                ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: tabs.map<Widget>(
+                    (tabName) {
+                      return Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: currentTab == tabName
+                                  ? const Color.fromARGB(255, 0, 115, 234)
+                                  : Colors.transparent,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(tabName),
+                      );
+                    },
+                  ).toList(),
+                ),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                child: Column(
+                  children: [
+                    switch (currentTab) {
+                      'Project Overview' => ProjectOverview(),
+                      'Scope & stack' => ScopeAndStack(),
+                      'Escalation matrix' => EscalationMatrix(),
+                      _ => Text(currentTab),
+                    },
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        switch (currentTab) {
+                          case 'Project Overview':
+                            setCurrentTab('Scope & stack');
+                          case 'Scope & stack':
+                            setCurrentTab('Escalation matrix');
+                          default:
+                            return;
+                        }
+                      },
+                      child: Text('Continue'),
+                      style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(
+                          Size(screenWidth, 50),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -293,6 +311,255 @@ class _ScopeAndStackState extends State<ScopeAndStack> {
             ),
             hintText: 'Write here',
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class EscalationMatrix extends StatefulWidget {
+  const EscalationMatrix({super.key});
+
+  @override
+  State<EscalationMatrix> createState() => _EscalationMatrixState();
+}
+
+class _EscalationMatrixState extends State<EscalationMatrix> {
+  List<String> tableHeaders = [
+    'Escalation Level',
+    'Name',
+    'Role',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Operational Escalation Matrix', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500,),),
+        EscalationMatrixTable(tableHeaders: tableHeaders),
+        SizedBox(height: 10,),
+        Text('Financial Escalation Matrix', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500,),),
+        EscalationMatrixTable(tableHeaders: tableHeaders),
+        SizedBox(height: 10,),
+        Text('Technical Escalation Matrix', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500,),),
+        EscalationMatrixTable(tableHeaders: tableHeaders),
+        
+      ],
+    );
+  }
+}
+
+class EscalationMatrixTable extends StatefulWidget {
+  const EscalationMatrixTable({
+    super.key,
+    required this.tableHeaders,
+  });
+  final List<String> tableHeaders;
+
+  @override
+  State<EscalationMatrixTable> createState() =>
+      _EscalationMatrixTableState();
+}
+
+class _EscalationMatrixTableState
+    extends State<EscalationMatrixTable> {
+  var listOfPersons = [
+    {
+      'name': 'Dipa Majumdar',
+      'role': 'Project Manager',
+    },
+    {
+      'name': 'Person 2',
+      'role': 'Role 2',
+    },
+    {
+      'name': 'Person 3',
+      'role': 'Role 3',
+    },
+  ];
+
+  String level1State = 'Dipa Majumdar';
+  String level2State = 'Dipa Majumdar';
+  String level3State = 'Dipa Majumdar';
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      border: TableBorder.symmetric(
+        outside: BorderSide(
+          color: Colors.grey.shade300,
+        ),
+      ),
+      children: [
+        TableRow(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey.shade400,
+              ),
+            ),
+          ),
+          children: widget.tableHeaders.map<Widget>(
+            (tableHeader) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6.0),
+                child: Text(
+                  tableHeader,
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              );
+            },
+          ).toList(),
+        ),
+        TableRow(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey.shade400,
+              ),
+            ),
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6.0),
+              child: Text(
+                'Level 1',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 2.0,),
+              child: DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down, size: 15,),
+                isExpanded: true,
+                style: TextStyle(
+                  fontSize: 12.5,
+                  color: Colors.black,
+                ),
+                items: listOfPersons.map<DropdownMenuItem>((person) {
+                  return DropdownMenuItem(
+                    value: person['name'],
+                    child: Text('${person['name']}'),
+                  );
+                },).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    level1State = value;
+                  });
+                },
+                value: level1State,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                listOfPersons.where((person) => person['name'] == level1State).first['role'] ?? 'Project Manager'
+              ),
+            ),
+
+          ],
+        ),
+        TableRow(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey.shade400,
+              ),
+            ),
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6.0),
+              child: Text(
+                'Level 2',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 2.0,),
+              child: DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down, size: 15,),
+                isExpanded: true,
+                style: TextStyle(
+                  fontSize: 12.5,
+                  color: Colors.black,
+                ),
+                items: listOfPersons.map<DropdownMenuItem>((person) {
+                  return DropdownMenuItem(
+                    value: person['name'],
+                    child: Text('${person['name']}'),
+                  );
+                },).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    level2State = value;
+                  });
+                },
+                value: level2State,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                listOfPersons.where((person) => person['name'] == level2State).first['role'] ?? 'Project Manager'
+              ),
+            ),
+
+          ],
+        ),
+        TableRow(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey.shade400,
+              ),
+            ),
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6.0),
+              child: Text(
+                'Level 3',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 2.0,),
+              child: DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down, size: 15,),
+                isExpanded: true,
+                style: TextStyle(
+                  fontSize: 12.5,
+                  color: Colors.black,
+                ),
+                items: listOfPersons.map<DropdownMenuItem>((person) {
+                  return DropdownMenuItem(
+                    value: person['name'],
+                    child: Text('${person['name']}'),
+                  );
+                },).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    level3State = value;
+                  });
+                },
+                value: level3State,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                listOfPersons.where((person) => person['name'] == level3State).first['role'] ?? 'Project Manager'
+              ),
+            ),
+
+          ],
         ),
       ],
     );
