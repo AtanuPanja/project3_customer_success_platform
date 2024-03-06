f674cafb56ff5c3912b390e25046fed4cb441df2
AtanuPanja <atanupanja9463@gmail.com>
2024-03-06 18:16:05 +0530

    Created a new data folder, and list_of_projects
    Moved the listOfProjects list, from
    ProjectListing widget, and defined a new data
    folder with a file named list_of_projects.dart,
    to keep the data. The data is then imported to
    assign to the state.

---
+++ lib/data/list_of_projects.dart
@@ -0,0 +1,79 @@
+var listOfProjects = [
+    {
+      'name': 'Food on time',
+      'start_date': '12 Feb 24',
+      'status': 'On going',
+      'project_manager': 'Dipa Majumdar',
+      'members': '0'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': '1 Jan 24',
+      'status': 'Closed',
+      'project_manager': 'Dipa Majumdar',
+      'members': '1'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Dipa Majumdar',
+      'members': '2'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Dipa Majumdar',
+      'members': '3'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Rohit Shah',
+      'members': '30'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Hold',
+      'project_manager': 'Rohit Shah',
+      'members': '20'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Rohit Shah',
+      'members': '13'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'In progress',
+      'project_manager': 'Rohit Shah',
+      'members': '9'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Closed',
+      'project_manager': 'Rohit Shah',
+      'members': '100'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Closed',
+      'project_manager': 'Rohit Shah',
+      'members': '100'
+    },
+    {
+      'name': '2023-01-01',
+      'start_date': 'Placeholder for start date',
+      'status': 'Closed',
+      'project_manager': 'Rohit Shah',
+      'members': '100'
+    },
+];
--- lib/widgets/projects_screen_widgets.dart
+++ lib/widgets/projects_screen_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project3_customer_success_platform/screens/project_detail_screen.dart';
+import '../data/list_of_projects.dart' as projects_data;
 
 class ProjectsCardDisplay extends StatelessWidget {
   const ProjectsCardDisplay({
@@ -60,85 +61,7 @@ class ProjectListing extends StatefulWidget {
 }
 
 class _ProjectListingState extends State<ProjectListing> {
-  var listOfProjects = [
-    {
-      'name': 'Food on time',
-      'start_date': '12 Feb 24',
-      'status': 'On going',
-      'project_manager': 'Dipa Majumdar',
-      'members': '0'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': '1 Jan 24',
-      'status': 'Closed',
-      'project_manager': 'Dipa Majumdar',
-      'members': '1'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Dipa Majumdar',
-      'members': '2'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Dipa Majumdar',
-      'members': '3'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Rohit Shah',
-      'members': '30'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Hold',
-      'project_manager': 'Rohit Shah',
-      'members': '20'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Rohit Shah',
-      'members': '13'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Rohit Shah',
-      'members': '9'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Closed',
-      'project_manager': 'Rohit Shah',
-      'members': '100'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Closed',
-      'project_manager': 'Rohit Shah',
-      'members': '100'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Closed',
-      'project_manager': 'Rohit Shah',
-      'members': '100'
-    },
-  ];
+  var listOfProjects = projects_data.listOfProjects;
 
   String selectedTab = 'All Projects';
   List<String> tabs = ['All Projects', 'In progress', 'Completed', 'Hold'];
