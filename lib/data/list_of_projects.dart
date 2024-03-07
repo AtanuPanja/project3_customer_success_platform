b61293efb72bfd03a51796ab761bc505b5b4169d
AtanuPanja <atanupanja9463@gmail.com>
2024-03-07 12:49:40 +0530

    Implemented addition of project
    The create project form after submission,
    would add the project to the current project
    listing. This update is the result of the change
    in the previous commits.
    
    Also, resolved two dart analyzer problems,
    by renaming snake_case identifier with camelCase
    identifier in the list_of_projects.dart file.

--- lib/data/list_of_projects.dart
+++ lib/data/list_of_projects.dart
@@ -1,97 +1,95 @@
 var listOfProjects = [
-    {
-      'name': 'Food on time',
-      'start_date': '12 Feb 24',
-      'status': 'On going',
-      'project_manager': 'Dipa Majumdar',
-      'members': '0'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': '1 Jan 24',
-      'status': 'Closed',
-      'project_manager': 'Dipa Majumdar',
-      'members': '1'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Dipa Majumdar',
-      'members': '2'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Dipa Majumdar',
-      'members': '3'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Rohit Shah',
-      'members': '30'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Hold',
-      'project_manager': 'Rohit Shah',
-      'members': '20'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Rohit Shah',
-      'members': '13'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'In progress',
-      'project_manager': 'Rohit Shah',
-      'members': '9'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Closed',
-      'project_manager': 'Rohit Shah',
-      'members': '100'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Closed',
-      'project_manager': 'Rohit Shah',
-      'members': '100'
-    },
-    {
-      'name': '2023-01-01',
-      'start_date': 'Placeholder for start date',
-      'status': 'Closed',
-      'project_manager': 'Rohit Shah',
-      'members': '100'
-    },
+  {
+    'name': 'Food on time',
+    'start_date': '12 Feb 24',
+    'status': 'On going',
+    'project_manager': 'Dipa Majumdar',
+    'members': '0'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': '1 Jan 24',
+    'status': 'Closed',
+    'project_manager': 'Dipa Majumdar',
+    'members': '1'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'In progress',
+    'project_manager': 'Dipa Majumdar',
+    'members': '2'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'In progress',
+    'project_manager': 'Dipa Majumdar',
+    'members': '3'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'In progress',
+    'project_manager': 'Rohit Shah',
+    'members': '30'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'Hold',
+    'project_manager': 'Rohit Shah',
+    'members': '20'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'In progress',
+    'project_manager': 'Rohit Shah',
+    'members': '13'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'In progress',
+    'project_manager': 'Rohit Shah',
+    'members': '9'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'Closed',
+    'project_manager': 'Rohit Shah',
+    'members': '100'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'Closed',
+    'project_manager': 'Rohit Shah',
+    'members': '100'
+  },
+  {
+    'name': '2023-01-01',
+    'start_date': 'Placeholder for start date',
+    'status': 'Closed',
+    'project_manager': 'Rohit Shah',
+    'members': '100'
+  },
 ];
 
 void addNewProject(
-  String name,
-  [
-    String start_date = '2023-01-01',
-    String status = 'In progress',
-    String project_manager = 'Dipa Majumdar',
-    String members = '2',
-  ]
-) {
+  String name, [
+  String startDate = '2023-01-01',
+  String status = 'In progress',
+  String projectManager = 'Dipa Majumdar',
+  String members = '2',
+]) {
   Map<String, String> newEntry = {
     'name': name,
-    'start_date': start_date,
+    'start_date': startDate,
     'status': status,
-    'project_manager': project_manager,
+    'project_manager': projectManager,
     'members': members,
   };
 
