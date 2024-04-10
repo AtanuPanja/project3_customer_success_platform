76d1ba8eafb45bbd99f4b2a77c77ddcd32f13b2c
AtanuPanja <atanupanja9463@gmail.com>
2024-04-10 18:56:19 +0530

    Use ChangeNotifierProvider
    wrap whole app with
    ChangeNotifierProvider
    to provide the provider state
    to all the children in the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project3_customer_success_platform/providers/projects_list_provider.dart';
+import 'package:provider/provider.dart';
 import 'screens/home_screen_layout.dart';
 import 'utils/themes.dart';
 
@@ -11,11 +13,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppThemes.lightTheme,
-      darkTheme: AppThemes.darkTheme,
-      debugShowCheckedModeBanner: false,
-      home: const SafeArea(child: HomeScreen()),
+    // wrapping the entire app with ChangeNotifierProvider
+    // single state - ProjectsList is required now, this is why ChangeNotifierProvider is used
+    return ChangeNotifierProvider(
+      create: (context) => ProjectsListProvider(),
+      child: MaterialApp(
+        theme: AppThemes.lightTheme,
+        darkTheme: AppThemes.darkTheme,
+        debugShowCheckedModeBanner: false,
+        home: const SafeArea(child: HomeScreen()),
+      ),
     );
   }
 }
+
+/*
+commit message:
+Use ChangeNotifierProvider
+
+Wrap MaterialApp with ChangeNotifierProvider
+to provide the state to all the children
+in the widget tree
+
+This acts as a global state
+Currently only a single state i.e. ProjectsList
+is required so use the ChangeNotifierProvider
+ */
