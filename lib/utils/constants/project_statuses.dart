946eb18acd6cd8b6775c7dfd6c832b8d0fd1257b
AtanuPanja <atanupanja9463@gmail.com>
2024-04-10 18:50:08 +0530

    Abstracted project status to a new class

---
+++ lib/utils/constants/project_statuses.dart
@@ -0,0 +1,8 @@
+// this class will provide the text status of the project
+class ProjectStatuses {
+  ProjectStatuses._();
+
+  static const String onGoing = 'On-Going';
+  static const String completed = 'Completed';
+  static const String hold = 'Hold';
+}
