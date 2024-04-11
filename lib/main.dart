235f5c898691df32eab97057d8ea1d8842e0024a
AtanuPanja <atanupanja9463@gmail.com>
2024-04-11 19:09:41 +0530

    Added CreateProjectProvider to main
    Used MultiProvider to have array of providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project3_customer_success_platform/providers/managers_list_provider.dart';
+import 'package:project3_customer_success_platform/providers/create_project_provider.dart';
 import 'package:project3_customer_success_platform/providers/projects_list_provider.dart';
 import 'package:provider/provider.dart';
 import 'screens/home_screen_layout.dart';
@@ -16,8 +16,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // wrapping the entire app with ChangeNotifierProvider
     // single state - ProjectsList is required now, this is why ChangeNotifierProvider is used
-    return ChangeNotifierProvider(
-      create: (context) => ProjectsListProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProjectsListProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CreateProjectProvider(),
+        ),
+      ],
       child: MaterialApp(
         theme: AppThemes.lightTheme,
         darkTheme: AppThemes.darkTheme,
@@ -27,16 +34,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-
-/*
-commit message:
-Use ChangeNotifierProvider
-
-Wrap MaterialApp with ChangeNotifierProvider
-to provide the state to all the children
-in the widget tree
-
-This acts as a global state
-Currently only a single state i.e. ProjectsList
-is required so use the ChangeNotifierProvider
- */
