681f858e6133721bd362c4122e7917f9219045bc
AtanuPanja <atanupanja9463@gmail.com>
2024-04-12 11:38:22 +0530

    Modified api base url for android
    It showed error in fetching data from
    localhost:8000, in android,
    read from a resource and changed the
    baseUrl

--- lib/utils/constants/api_endpoints.dart
+++ lib/utils/constants/api_endpoints.dart
@@ -1,4 +1,5 @@
 import 'api_role_constants.dart';
+import 'dart:io' show Platform;
 
 // this class will provide all the endpoints to use for the fetching of data
 // currently this contains only the get endpoints
@@ -6,7 +7,12 @@ class ApiEndpoints {
 
   ApiEndpoints._();
 
-  static const String baseUrl = "http://localhost:8000";
+  // in android the server would run at http://10.0.2.2:8000. So, this was necessary, as otherwise it won't fetch the data in android system
+  // in ios system localhost:8000 works fine.
+  // Reference: https://stackoverflow.com/questions/55785581/socketexception-os-error-connection-refused-errno-111-in-flutter-using-djan
+  static final String baseUrl = Platform.isAndroid ? "http://10.0.2.2:8000": "http://localhost:8000";
+  // Platform class from dart:io package helps us recognize the platform the app is built on
+
   static const String getManagers = "/getManagers?role_id=rol_qLO42FIvSNsdZEO4";
   static const String getAllProjects =
       "/projects?role=${ApiRoleConstants.admin}&id=${ApiRoleConstants.adminID}";
